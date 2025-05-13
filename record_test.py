4df4ba7dcde14f531b9ea958c15d3cefb7003c66
AshiqueRS <ashiq.cse24@gmail.com>
2025-05-13 20:45:10 +0600

    Successful Login test - Positive case

--- record_test.py
+++ record_test.py
@@ -1,23 +1,26 @@
-import re
-from playwright.sync_api import Playwright, sync_playwright, expect
-
-
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://www.saucedemo.com/")
-    page.locator("[data-test=\"username\"]").click()
-    page.locator("[data-test=\"username\"]").fill("standard_user")
-    page.locator("[data-test=\"username\"]").press("Tab")
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
-    page.locator("[data-test=\"login-button\"]").click()
-
-    assert page.is_visible("text=Swag Labs")
-    # ---------------------
-    context.close()
-    browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
+# from playwright.sync_api import Playwright, sync_playwright, expect
+#
+# def test_successful_login(playwright: Playwright) -> None :
+#     # Browser Launch
+#     browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+#     #to create a new session in browser
+#     context = browser.new_context()
+#     #to open a new page
+#     page = context.new_page()
+#     #to go to the website
+#     page.goto("https://www.saucedemo.com/")
+#
+#     #fill the field with credential for log in
+#     page.fill("#user-name", "standard_user")
+#     page.fill("#password", "secret_sauce")
+#     page.click("#login-button")
+#
+#     # Verify Login is successful
+#     expect
+#
+#
+#     browser.close()
+#
+#
+# with sync_playwright() as playwright:
+#     test_successful_login(playwright)
---
+++ testcases.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_successful_login(playwright: Playwright) -> None :
+    # Browser Launch
+    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+    #to create a new session in browser
+    context = browser.new_context()
+    #to open a new page
+    page = context.new_page()
+    #to go to the website
+    page.goto("https://www.saucedemo.com/")
+
+    #fill the field with credential for log in
+    page.fill("#user-name", "standard_user")
+    page.fill("#password", "secret_sauce")
+    page.click("#login-button")
+
+    # Verify Login is successful 1
+    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+
+    # Verify Login is successful 2
+    expect(page.locator("#shopping_cart_container")).to_be_visible()
+
+    print("✅ Login Test Passed")
+
+
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    test_successful_login(playwright)
